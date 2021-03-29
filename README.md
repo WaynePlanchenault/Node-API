72549c5b0c2dd8f6c3b6ed87abb79717c86b6b75
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-03-29 13:56:57 +0200

    cors initialisation

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ npm i -s express // instalation du microframworks express
 npm i -s mongoose // Mongoose est un outil de modélisation d'objets MongoDB conçu pour fonctionner dans un environnement asynchrone.
 
 npm i -s body-parser // permet d'interpréter du JSON
+
+npm i -s cors // permet de rendre l'API accessible n'importe où
--- index.js
+++ index.js
@@ -5,12 +5,14 @@ require('./models/dbConfig');
 const postsRoutes = require('./routes/postsController');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.set('useFindAndModify' , false)
 
 
 /* MIDDLEWARE */ 
 app.use(bodyParser.json());
+app.use(cors(/* {origin: 'https://facebook.com'}  */)) //donne l'accès à notre API à n'importe qui ou à un site en particulier
 app.use('/posts', postsRoutes)
 // quand le chemin est / tu nous envoies à postsRoutes
 //un middleware est une fonction qui va écouter les request et les responses 
